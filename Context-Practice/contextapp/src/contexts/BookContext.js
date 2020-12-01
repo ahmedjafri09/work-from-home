e2d731469d4fbff4d32618df2f3342cb7c43dbf9
Syed M. Ahmed Jafri <53001314+ahmedjafri09@users.noreply.github.com>
2020-12-01 18:21:24 +0500

    fixed some problems

--- Context-Practice/contextapp/src/contexts/BookContext.js
+++ Context-Practice/contextapp/src/contexts/BookContext.js
@@ -1,25 +1,25 @@
-import React, { createContext, useState } from 'react';
-import uuid from 'uuid/v1';
+import React, { createContext, useState } from "react";
+import v1 from "uuid/dist/v1";
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
   const [books, setBooks] = useState([
-    {title: 'name of the wind', author: 'patrick rothfuss', id: 1},
-    {title: 'the final empire', author: 'brandon sanderson', id: 2},
+    { title: "name of the wind", author: "patrick rothfuss", id: 1 },
+    { title: "the final empire", author: "brandon sanderson", id: 2 },
   ]);
   const addBook = (title, author) => {
-    setBooks([...books, {title, author, id: uuid()}]);
+    setBooks([...books, { title, author, id: v1() }]);
   };
   const removeBook = (id) => {
-    setBooks(books.filter(book => book.id !== id));
-  }
+    setBooks(books.filter((book) => book.id !== id));
+  };
 
   return (
     <BookContext.Provider value={{ books, addBook, removeBook }}>
       {props.children}
     </BookContext.Provider>
   );
-}
- 
+};
+
 export default BookContextProvider;
