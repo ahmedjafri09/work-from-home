9c2ffdb7cd0ffb35800126c9a52d314e07bb42c2
Syed M. Ahmed Jafri <53001314+ahmedjafri09@users.noreply.github.com>
2020-12-03 18:05:26 +0500

    event raising and listener

--- NodeJS-ExpressJS/Node/firstApp/app.js
+++ NodeJS-ExpressJS/Node/firstApp/app.js
@@ -1,3 +1,25 @@
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
 const logger = require("./logger");
 
-logger.hey("Ahmed");
+//Register the listener before you raise an event ALWAYS.
+//The reason is that when an event is raise, the compiler
+//will check for all the registered listeners and match and
+//execute the one which matches.
+
+//Register an event listener
+emitter.on("messageLogged", (eventArgs) => {
+  console.log("There was an event: ", eventArgs);
+});
+
+//Raise an event
+emitter.emit("messageLogged", { id: 1, url: `The URL` });
+
+const name = logger.hey("Ahmed");
+
+//Raising and registering an event listener for logger:
+emitter.on(name, (eventArgs) => {
+  console.log("Logger function", eventArgs);
+});
+
+emitter.emit(name, { id: 1, name: "name" });
