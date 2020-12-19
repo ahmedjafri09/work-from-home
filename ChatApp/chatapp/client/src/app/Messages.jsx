e623918fe58729ba19dc9f9fd9f8dad42dc62c2b
Syed M. Ahmed Jafri <53001314+ahmedjafri09@users.noreply.github.com>
2020-12-20 03:45:05 +0500

    setting front-end

--- ChatApp/chatapp/client/src/app/Messages.jsx
+++ ChatApp/chatapp/client/src/app/Messages.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import './messages.css';
-import dayjs from 'dayjs';
-
+import React from "react";
+import "./messages.css";
+import dayjs from "dayjs";
+// import { useState } from "react";
 
 const Messages = (props) => {
-    const { sender } = props;
-    let classname;
-    let time = dayjs().format('hh:mm A');
-    // let currTime = time.getHours() + ":" + time.getMinutes();
-    sender === true ? classname = 'message-sender' : classname = 'message-reciever';
-    return (
-        <div className={classname}>
-            Lorem ipsum dolor sit. Quaerat porro corrupti ex est.
-            <p className='text-muted'>{time}</p>
-        </div>);
-}
+  //   const { message, setMessage } = useState([]);
+  const { sender, message } = props;
+  let classname;
+  let time = dayjs().format("hh:mm A");
+  // let currTime = time.getHours() + ":" + time.getMinutes();
+  sender === true
+    ? (classname = "message-sender")
+    : (classname = "message-reciever");
+  return (
+    <div className={classname}>
+      {message}
+      <p className="text-muted">{time}</p>
+    </div>
+  );
+};
 
 export default Messages;
